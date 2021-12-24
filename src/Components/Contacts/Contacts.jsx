7a7af72da5b924365eda9443eef08a633c835dce
Настя <ablovatskaja@gmail.com>
2021-12-24 11:14:44 +0300

    Add contacts

--- src/Components/Contacts/Contacts.jsx
+++ src/Components/Contacts/Contacts.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import style from './Contacts.module.css';
 import styleContainer from "../../Common/Styles/Container.module.css";
 import {Title} from "../Title/Title";
+import TextField from "@mui/material/TextField";
 
 export const Contacts = () => {
     return (
@@ -9,6 +10,9 @@ export const Contacts = () => {
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
             <Title title={'Contact'}/>
             <form className={style.form}>
+                <TextField id="standard-basic" label="Standard" variant="standard" />
+                <TextField id="standard-basic" label="Standard" variant="standard" />
+                <TextField id="standard-basic" label="Standard" variant="standard" />
                 <input type='text' placeholder='Your name' className={style.input}/>
                 <input type='email' placeholder='Your email' className={style.input}/>
                 <textarea  placeholder='Your message' className={style.input}></textarea>
